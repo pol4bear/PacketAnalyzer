9f644f33d59c8e9268b2d97538c6f208248f91f3
root <khhis@naver.com>
2019-07-16 09:31:01 -0700

    packet.cpp, packet.h Modified

--- packet.cpp
+++ packet.cpp
@@ -3,20 +3,32 @@
 Packet transPacket(const u_char *packetIn)
 {
     Packet ret;
+    int translatedBytes = 0;
 
-    ret.ethernet.destMac = &packet[8];
-    ret.ethernet.destMac[1] = packet[9];
-    ret.ethernet.destMac[2] = packet[10];
-    ret.ethernet.destMac[3] = packet[11];
-    ret.ethernet.destMac[4] = packet[12];
-    ret.ethernet.destMac[5] = packet[13];
-    ret.ethernet.srcMac[0] = packet[14];
-    ret.ethernet.srcMac[1] = packet[15];
-    ret.ethernet.srcMac[2] = packet[16];
-    ret.ethernet.srcMac[3] = packet[17];
-    ret.ethernet.srcMac[4] = packet[18];
-    ret.ethernet.srcMac[5] = packet[19];
-    ret.ethernet.type =
+    u_short preamble = *packetIn;
+
+    if(preamble == PREAMBLE)
+    {
+        translatedBytes += SIZE_PREAMBLE;
+        ret.ethernet = (Ethernet*)&packetIn[translatedBytes];
+        translatedBytes += SIZE_ETHERNET;
+
+
+        if(ret.ethernet->type == TYPE_IP)
+        {
+            ret.ip = (IP*)&packetIn[translatedBytes];
+            translatedBytes += ret.ip->headerLength;
+
+            if(ret.ip->protocol == PROTOCOL_TCP)
+            {
+                ret.tcp = (TCP*)&packetIn[translatedBytes];
+                translatedBytes += ret.tcp->headerLength;
+
+                ret.payload_len = ret.ip->totalLength - ret.ip->headerLength - ret.tcp->headerLength;
+                ret.payload = &packetIn[translatedBytes];
+            }
+        }
+    }
 
     return ret;
 }
--- packet.h
+++ packet.h
@@ -3,37 +3,68 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 
-#define ETHERNET_SIZE = 14
-#define MAC_SIZE 6
+#define SIZE_PREAMBLE 8
+#define SIZE_ETHERNET 22
+
+#define PREAMBLE 0xAAAB
+
+#define TYPE_IP 0x0800
+
+#define PROTOCOL_TCP 0x6
+
+#define TCP_NONCE 0x100
+#define TCP_CONGESTION 0x80
+#define TCP_ECN 0x40
+#define TCP_URGENT 0x20
+#define TCP_ACK 0x10
+#define TCP_PUSH 0x8
+#define TCP_RESET 0x4
+#define TCP_SYN 0x2
+#define TCP_FIN 0x1
 
 typedef struct __ETHERNET__
 {
-    u_char *destMac;
-    u_char *srcMac;
-    u_short *type;
+    u_short destMac : 6;
+    u_short srcMac : 6;
+    u_short type;
 }Ethernet;
 
 typedef struct __IP__
 {
-
+    u_char version : 4;
+    u_char headerLength : 4;
+    u_char serviceType;
+    u_short totalLength;
+    u_short identification;
+    u_char flags : 3;
+    u_short fragmentOffset : 13;
+    u_char timeToLive;
+    u_char protocol;
+    u_short headerChecksum;
+    u_int sourceIP;
+    u_int destIP;
 } IP;
-
 typedef struct __TCP__
 {
-
+    u_short sourcePort;
+    u_short destPort;
+    u_int sequenceNumber;
+    u_int acknowledgeNumber;
+    u_char headerLength : 4;
+    u_char reserved : 3;
+    u_int flags : 9;
+    u_short windowSize;
+    u_short checksum;
+    u_short urgentPointer;
 } TCP;
-
-typedef struct __APPLICATION__
-{
-
-} Application;
 
 typedef struct __PACKET__
 {
-    Ethernet ethernet;
-    IP ip;
-    TCP tcp;
-    Application app;
+    Ethernet *ethernet;
+    IP *ip;
+    TCP *tcp;
+    u_short payload_len;
+    const u_char *payload;
 } Packet;
 
 Packet transPacket(const u_char *packetIn);
